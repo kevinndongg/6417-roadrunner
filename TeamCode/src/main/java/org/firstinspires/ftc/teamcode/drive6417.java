8bef84ac520d3a0ac89526f946498d90959d2905
kevinndongg <kevin.dong.haonan@gmail.com>
2023-02-09 16:05:46 -0500

    drive class started

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive6417.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+public class drive6417 extends SampleMecanumDrive {
+    DcMotorEx frontLeft, frontRight, backLeft, backRight;
+}
