9d41c7ca3b8a34f3f46bb45347ab998a03647260
kevinndongg <kevin.dong.haonan@gmail.com>
2023-03-02 13:04:54 -0500

    constants made final

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,38 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Constants {
-    public static double grabberClose = 0.65;
-    public static double grabberOpen = 0.83;
+    public static final double grabberClose = 0.65;
+    public static final double grabberOpen = 0.83;
 
-    public static double wristDown = 0.91;
-    public static double wristUp = 0.29;
+    public static final double wristDown = 0.91;
+    public static final double wristUp = 0.29;
 
-    public static double armSlowPower = 0.4;
-    public static double armFastPower = 0.7;
-    public static int armNear = 300;
-    public static int armBackUpPos = 1050;
-    public static int armBackLowPos = 1160;
-    public static int armBackGroundPos = 1850;
-    public static int armDunk = 170;
-    public static int slideMediumPos = 600;
-    public static int slideHighPos = 1450;
-    public static int slideBobPos = 150;
-    public static int slideNearBottomPos = 200;
-    public static double slideDownPower = 0.95;
-    public static double slideUpPower = 0.75;
-    public static double slideBobPower = 0.25;
+    public static final double armSlowPower = 0.4;
+    public static final double armFastPower = 0.7;
+    public static final int armNear = 300;
+    public static final int armBackUpPos = 1050;
+    public static final int armBackLowPos = 1160;
+    public static final int armBackGroundPos = 1850;
+    public static final int armDunk = 170;
+    public static final int slideMediumPos = 600;
+    public static final int slideHighPos = 1450;
+    public static final int slideBobPos = 150;
+    public static final int slideNearBottomPos = 200;
+    public static final double slideDownPower = 0.95;
+    public static final double slideUpPower = 0.75;
+    public static final double slideBobPower = 0.25;
 
-    public static double driveSpeedIntake = 0.6;
-    public static double driveSpeedIntakeSlow = 0.35;
-    public static double driveSpeedManeuvering = 0.7;
-    public static double driveSpeedManeuveringSlow = 0.5;
-    public static double driveSpeedOuttakeUp = 0.4;
-    public static double driveSpeedOuttakeUpSlow = 0.25;
-    public static double driveSpeedOuttakeGround = 0.55;
-    public static double driveSpeedOuttakeGroundSlow = 0.35;
+    public static final double driveSpeedIntake = 0.6;
+    public static final double driveSpeedIntakeSlow = 0.35;
+    public static final double driveSpeedManeuvering = 0.7;
+    public static final double driveSpeedManeuveringSlow = 0.5;
+    public static final double driveSpeedOuttakeUp = 0.4;
+    public static final double driveSpeedOuttakeUpSlow = 0.25;
+    public static final double driveSpeedOuttakeGround = 0.55;
+    public static final double driveSpeedOuttakeGroundSlow = 0.35;
 
-    public static double driveTuningFR = 1.0;
-    public static double driveTuningFL = 0.95;
-    public static double driveTuningBR = 1.0;
-    public static double driveTuningBL = 0.95;
+    public static final double driveTuningFR = 1.0;
+    public static final double driveTuningFL = 0.95;
+    public static final double driveTuningBR = 1.0;
+    public static final double driveTuningBL = 0.95;
 }
